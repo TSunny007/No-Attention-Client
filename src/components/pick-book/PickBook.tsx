a19252b638609eb757985a32b3ad8f5a326102f2
Nithin Chalapathi <nithin.ch10@gmail.com>
2019-10-05 19:38:21 -0600

    card styling doen

--- src/components/pick-book/PickBook.tsx
+++ src/components/pick-book/PickBook.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, Row, Col } from 'react-bootstrap';
 import { IBook } from '../../Globals';
+import { card_div } from '../../css';
 
 export default class PickBook extends React.Component {
     books: Array<IBook>;
@@ -8,32 +9,50 @@ export default class PickBook extends React.Component {
     constructor(public props: Readonly<any>) {
         super(props);
         this.books = [
-            { name: 'Cat In the Hat', 
-              author: 'Dr. Seuss',
-              description: 'This book is about a cat in the hat. A very popular and widely loved book.'},
-            { name: 'Fox in Socks' , 
-            author: 'Dr. Seuss',
-            description: 'This book is about a Fox in socks. A very popular and widely loved book.'},
-            { name: 'How the Grinch Stole Christmas', 
-            author: 'Dr. Seuss' ,
-            description: 'This book is about a grinch. A very popular and widely loved book.'}]
+            {
+                name: 'Cat In the Hat',
+                author: 'Dr. Seuss',
+                description: 'This book is about a cat in the hat. A very popular and widely loved book.'
+            },
+            {
+                name: 'Fox in Socks',
+                author: 'Dr. Seuss',
+                description: 'This book is about a Fox in socks. A very popular and widely loved book.'
+            },
+            {
+                name: 'How the Grinch Stole Christmas',
+                author: 'Dr. Seuss',
+                description: 'This book is about a grinch. A very popular and widely loved book.'
+            },
+        ]
+    }
+
+    public renderBookCard(book: IBook) {
+        return (
+            <Card style={{ width: '18rem' }}>
+                {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+                <Card.Body>
+                    <Card.Title>{book.name}</Card.Title>
+                    <Card.Text>
+                        {book.description}
+                    </Card.Text>
+                    <Button variant="primary" href={book.name}>Read this book!</Button>
+                </Card.Body>
+            </Card>
+        );
     }
 
     public render() {
         return (
-            <div>
-                {this.books.map((book: IBook) => {
-                    return (<Card style={{ width: '18rem' }}>
-                        {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-                        <Card.Body>
-                            <Card.Title>{book.name}</Card.Title>
-                            <Card.Text>
-                                {book.description}
-                            </Card.Text>
-                            <Button variant="primary" href={book.name}>Read this book!</Button>
-                        </Card.Body>
-                    </Card>)
-                })}
+            <div className={card_div}>
+                <Row>
+                    {this.books.map((book: IBook, i: number) => {
+                        return (
+                        <Col key={i}>
+                            {this.renderBookCard(book)}
+                        </Col>);
+                    })}
+                </Row>
             </div>
         );
     }
---
+++ src/css.tsx
@@ -0,0 +1,7 @@
+import { css } from 'emotion';
+
+export var card_div: any = css({
+    'marginLeft': '10%',
+    'marginRight': '10%',
+    'marginTop': '10px'  
+});
